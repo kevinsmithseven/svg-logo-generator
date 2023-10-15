8dd461e665a0dbe5ffe794ff7497c7b9c7b553d8
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-15 11:35:16 -0500

    updated index.js to pass user input into generateLogo function and required the generateLogo function from shapes.js. Added functionality to generate logo.svg after return of promise.Changed name of shapes.js to Shapes.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const { promptUser } = require('./lib/userInput')
+// Import the promptUser function from userinput.js
+const { promptUser } = require('./lib/userinput')
+
+// Utilize promises for promptUser
+const { writeFile } = require('fs').promises;
+
+// Import generateLogo function from shapes.js
+const { generateLogo} = require('./lib/Shapes');
+
+const init = () => {
+    promptUser()
+
+    //Pass user input from userinput.js into generateLogo function on shapes.js
+    .then((userInput) => {
+        return generateLogo(userInput);
+    })
+    .then((logoContent) => {
+        // Write content to the logo.svg file
+        return writeFile('./output/logo.svg', logoContent)
+    })
+    .then(() => console.log('Successfully created logo.svg'))
+    .catch((err) => console.error(err));   
+};
+
+init();
